c44468707ba8235852d72045d09941eb930a5b8e
Dustin Ray <40841027+dustinvonsandwich@users.noreply.github.com>
2020-04-26 22:03:37 -0700

    should be able to handle 16 bit blocks

--- salsa20.py
+++ salsa20.py
@@ -6,9 +6,8 @@ ROUNDS = 20
 #rotating function
 def ROTL(b, r):
 
-    s = ( ( ( b << r )) | ( b >> ( 32 - r ) ) ) 
+    s = (((b << r)) | (b >> (32 - r))) 
     return s
-
 
 # quarter round function
 def QR(a, b, c, d):
@@ -19,31 +18,31 @@ def QR(a, b, c, d):
     a ^= ROTL(d + c, 18)
 
 
-def init(nonce, key, counter):
+# salsa20 algorithm takes in_16 as input which is 16 bit
+# plaintext to be encrypted
+def main(in_16):
 
-    nonce = [0]*8
-    key = [0]*32
-    counter = [0]*8
-
-    n = [nonce[4 * i : 4 * i + 4] for i in range(2)]
-    k = [(key[4*i:4*i+4]) for i in range(8)]
-    b = [(counter[4*i:4*i+4]) for i in range(2)]
-
-
-    in0 = [16]
+    in_16 = [16]
     x = [16]
     i = 0
 
     while i < ROUNDS:
         
-        # run quarter round alg on columns here
+        # run quarter round alg on columns
         QR(x[0], x[4], x[8], x[12]) 
         QR(x[5], x[9], x[13], x[1])
         QR(x[10], x[14], x[2], x[6])
         QR(x[15], x[3], x[7], x[11])
 
-        # run QR alg on rows here
+        # run QR alg on rows
         QR(x[ 0], x[ 1], x[ 2], x[ 3])
         QR(x[ 5], x[ 6], x[ 7], x[ 4])
         QR(x[10], x[11], x[ 8], x[ 9])
         QR(x[15], x[12], x[13], x[14])
+
+        i += 2
+
+    # final loop combines original input with
+    # ciphertext to create encrypted cipher text
+    for el in x:
+        x[el] + in_16[el]
